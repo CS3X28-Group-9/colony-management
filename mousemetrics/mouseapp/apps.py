a2d82371f1af895292bb494ec232f4aab39c2810
mhannah04 <157507841+mhannah04@users.noreply.github.com>
2025-10-06 23:41:52 +0100

    Pre-commit workflow; CI workflow

--- mousemetrics/mouseapp/apps.py
+++ mousemetrics/mouseapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MouseappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mouseapp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mouseapp"
