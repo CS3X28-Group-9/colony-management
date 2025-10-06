1b144aad1f3c70bc98b15a069a215d6fa8e1e50c
mhannah04 <157507841+mhannah04@users.noreply.github.com>
2025-10-06 23:41:52 +0100

    Pre-commit workflow; CI workflow

--- mousemetrics/mousemetrics/wsgi.py
+++ mousemetrics/mousemetrics/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mousemetrics.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mousemetrics.settings")
 
 application = get_wsgi_application()
