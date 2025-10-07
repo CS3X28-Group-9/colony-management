d42e51832fc9df21cce22105801fa12a59d63626
mhannah04 <157507841+mhannah04@users.noreply.github.com>
2025-10-07 19:18:09 +0100

    implemented basic tests, rebased with main

--- mousemetrics/mouseapp/tests.py
+++ mousemetrics/mouseapp/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_index_gives_correct_message(client):
+    response = client.get(reverse("mouseapp-hello"))
+    assert response.status_code == 200
+    assert response.content.decode() == "Hello, world. You're at the mouseapp index."
+
+
+@pytest.mark.django_db
+def test_index_gives_correct_content_type(client):
+    response = client.get(reverse("mouseapp-hello"))
+    assert response["Content-Type"] == "text/html; charset=utf-8"
