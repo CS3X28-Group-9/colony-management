b191fb05047f7fdc4f23ff1007039dc69fbcbf96
Moth Robson <94545082+Spartan2909@users.noreply.github.com>
2025-10-09 11:08:31 +0100

    Add models to admin

--- mousemetrics/mouseapp/admin.py
+++ mousemetrics/mouseapp/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Box, Mouse, Project, Request
+
+admin.site.register(Box)
+admin.site.register(Mouse)
+admin.site.register(Project)
+admin.site.register(Request)
