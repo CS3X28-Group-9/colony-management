a14cfae0fbfa440fe49e73b00a1fda172e1a88f7
stephnos <stephanos@papatsaras.com>
2025-10-10 12:07:39 +0100

    added basic signup page

--- mousemetrics/mouseapp/urls.py
+++ mousemetrics/mouseapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, SignUp
 
 urlpatterns = [
     path("", home, name="home"),
+    path("signup/", SignUp.as_view(), name="signup"),
 ]
--- mousemetrics/mouseapp/views.py
+++ mousemetrics/mouseapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 def home(request):
     return render(request, "home.html")
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
