a3ad0970903618cfa411be1457793d02a438a228
Moth Robson <94545082+Spartan2909@users.noreply.github.com>
2025-10-16 11:31:07 +0100

    Add missing model to admin

--- mousemetrics/mouseapp/admin.py
+++ mousemetrics/mouseapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Box, Mouse, Project, Request
+from .models import Box, Membership, Mouse, Project, Request
 
 admin.site.register(Box)
+admin.site.register(Membership)
 admin.site.register(Mouse)
 admin.site.register(Project)
 admin.site.register(Request)
