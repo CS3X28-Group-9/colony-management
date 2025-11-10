172925f9752b95244eb29d73a7e36fd3a55a47bc
Moth Robson <94545082+Spartan2909@users.noreply.github.com>
2025-11-10 14:13:46 +0000

    Add study plan to admin

--- mousemetrics/mouseapp/admin.py
+++ mousemetrics/mouseapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Box, Membership, Mouse, Project, Request
+from .models import Box, Membership, Mouse, Project, Request, StudyPlan
 
 admin.site.register(Box)
 admin.site.register(Membership)
 admin.site.register(Mouse)
 admin.site.register(Project)
 admin.site.register(Request)
+admin.site.register(StudyPlan)
