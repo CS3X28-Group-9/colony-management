0dfe9e1d72554c97bb39d6ab9c05624e11690a08
mhannah04 <157507841+mhannah04@users.noreply.github.com>
2026-01-30 19:58:20 +0000

    removed pyright comments

--- mousemetrics/mouseapp/tests.py
+++ mousemetrics/mouseapp/tests.py
@@ -91,10 +91,10 @@ def test_user_creation():
     assert user is not None
 
     db_user = User.objects.get(email=user_data["email"])
-    assert db_user.email == user_data["email"]  # type: ignore reportUnknownArgumentType
-    assert db_user.first_name == user_data["first_name"]  # type: ignore reportUnknownMemberType
-    assert db_user.last_name == user_data["last_name"]  # type: ignore reportUnknownMemberType
-    assert db_user.check_password(user_data["password1"])  # type: ignore reportUnknownMemberType
+    assert db_user.email == user_data["email"]
+    assert db_user.first_name == user_data["first_name"]
+    assert db_user.last_name == user_data["last_name"]
+    assert db_user.check_password(user_data["password1"])
 
 
 @pytest.mark.django_db
@@ -113,8 +113,8 @@ def test_login_redirect(client: Client):
     response = client.post(
         login_url,
         {
-            "username": user.email,  # pyright: ignore reportUnknownMemberType
-            "password": password,  # pyright: ignore reportUnknownMemberType
+            "username": user.email,
+            "password": password,
         },
     )
 
@@ -170,7 +170,7 @@ def test_create_breeding_request_requires_login(client: Client):
     url = reverse("mouseapp:create_breeding_request")
     response = client.get(url)
     assert response.status_code == 302
-    assert "login" in response["Location"].lower()  # pyright: ignore
+    assert "login" in response["Location"].lower()
 
 
 @pytest.mark.django_db
@@ -334,7 +334,7 @@ def test_requests_page_requires_login(client: Client):
     url = reverse("mouseapp:requests")
     response = client.get(url)
     assert response.status_code == 302
-    assert "login" in response["Location"].lower()  # pyright: ignore
+    assert "login" in response["Location"].lower()
 
 
 @pytest.mark.django_db
